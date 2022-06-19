2b8a8184d4fa2c7ae818f84395dca6ca0a84be83
iceBao <2567430143@qq.com>
2022-06-19 13:09:07 +0800

    配置

--- Unity/Assets/Mono/Core/Helper/MD5Helper.cs
+++ Unity/Assets/Mono/Core/Helper/MD5Helper.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace ET
 {
@@ -14,6 +16,14 @@ namespace ET
 				retVal = md5.ComputeHash(file);
 			}
 			return retVal.ToHex("x2");
+		}
+
+		public static string StringMD5(string content)
+		{
+			MD5 md5 = MD5.Create();
+			byte[] result = Encoding.Default.GetBytes(content);
+			byte[] output = md5.ComputeHash(result);
+			return BitConverter.ToString(output).Replace("-", "");
 		}
 	}
 }
