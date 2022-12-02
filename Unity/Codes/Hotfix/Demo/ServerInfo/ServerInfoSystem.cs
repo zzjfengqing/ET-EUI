67781a88073b866aba2329ea6535202cad232b7c
缘缘 <1253301988@qq.com>
2022-12-02 16:00:17 +0800

    ServerInfo

---
+++ Unity/Codes/Hotfix/Demo/ServerInfo/ServerInfoSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public static class ServerInfoSystem
+    {
+        public static void FromMessage(this ServerInfo self, NServerInfo nServerInfo)
+        {
+            self.Id = nServerInfo.Id;
+            self.Status = nServerInfo.Status;
+            self.ServerName = nServerInfo.ServerName;
+        }
+
+        public static NServerInfo ToMessage(this ServerInfo self)
+        {
+            return new NServerInfo()
+            {
+                Id = (int)self.Id,
+                Status = self.Status,
+                ServerName = self.ServerName,
+            };
+        }
+    }
+}
