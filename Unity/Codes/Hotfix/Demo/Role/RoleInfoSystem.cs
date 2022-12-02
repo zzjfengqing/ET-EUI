14fea03656456a440817da07affd090d11878e05
缘缘 <1253301988@qq.com>
2022-12-02 20:24:54 +0800

    RoleInfoSystem

---
+++ Unity/Codes/Hotfix/Demo/Role/RoleInfoSystem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public static class RoleInfoSystem
+    {
+        public static void FromNServerInfo(this RoleInfo self, NRoleInfo nServerInfo)
+        {
+            self.Id = nServerInfo.Id;
+            self.Name = nServerInfo.Name;
+            self.ServerId = nServerInfo.ServerId;
+            self.Status = nServerInfo.Status;
+            self.AccountId = nServerInfo.AccountId;
+            self.LastLoginTIme = nServerInfo.LastLoginTIme;
+            self.CreateTime = nServerInfo.CreateTime;
+        }
+
+        public static NRoleInfo ToNServerInfo(this RoleInfo self)
+        {
+            return new NRoleInfo()
+            {
+                Id = self.Id,
+                Name = self.Name,
+                ServerId = self.ServerId,
+                Status = self.Status,
+                AccountId = self.AccountId,
+                LastLoginTIme = self.LastLoginTIme,
+                CreateTime = self.CreateTime,
+            };
+        }
+    }
+}
