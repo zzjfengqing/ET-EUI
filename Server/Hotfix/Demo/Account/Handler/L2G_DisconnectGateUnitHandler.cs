da0d334a5c87413bf18fc8f00fc8f2a7f771ea6b
缘缘 <1253301988@qq.com>
2022-12-02 14:33:26 +0800

    L2G_DisconnectGateUnitHandler

---
+++ Server/Hotfix/Demo/Account/Handler/L2G_DisconnectGateUnitHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ET
+{
+    public class L2G_DisconnectGateUnitHandler : AMActorRpcHandler<Scene, L2G_DisconnectGateUnit, G2L_DisconnectGateUnit>
+    {
+        protected override async ETTask Run(Scene scene, L2G_DisconnectGateUnit request, G2L_DisconnectGateUnit response, Action reply)
+        {
+            var accountId = request.AccountId;
+            using (await CoroutineLockComponent.Instance.Wait(CoroutineLockType.GateLoginLock, accountId.GetHashCode()))
+            {
+                PlayerComponent playerComponent = scene.GetComponent<PlayerComponent>();
+                var player = playerComponent.Get(accountId);
+                if (player == null)
+                {
+                    reply();
+                    return;
+                }
+                playerComponent.Remove(accountId);
+                //Todo:强制玩家下线
+                player.Dispose();
+            }
+            reply();
+        }
+    }
+}
