5346989c71ec62f62101b84e76044384186ff3da
缘缘 <1253301988@qq.com>
2022-12-05 00:41:57 +0800

    G2M_RequestEnterGameStatusHandler

---
+++ Server/Hotfix/Demo/Account/Handler/G2M_RequestEnterGameStatusHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public class G2M_RequestEnterGameStatusHandler : AMActorLocationRpcHandler<Unit, G2M_RequestEnterGameStatus, M2G_RequestEnterGameStatus>
+    {
+        protected override async ETTask Run(Unit unit, G2M_RequestEnterGameStatus request, M2G_RequestEnterGameStatus response, Action reply)
+        {
+            reply();
+            await ETTask.CompletedTask;
+        }
+    }
+}
