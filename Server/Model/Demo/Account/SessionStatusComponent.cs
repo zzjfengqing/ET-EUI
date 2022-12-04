ae34d6c1eec5332ef5bb2fe672dd35e97c494549
缘缘 <1253301988@qq.com>
2022-12-05 00:10:46 +0800

    SessionLoginComponent

---
+++ Server/Model/Demo/Account/SessionStatusComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public enum SessionStatus
+    {
+        Normal,
+        Game
+    }
+
+    [ComponentOf(typeof(Session))]
+    public class SessionStatusComponent : Entity, IAwake
+    {
+        public SessionStatus Status;
+    }
+}
