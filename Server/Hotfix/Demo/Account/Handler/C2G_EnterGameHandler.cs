3c2d53d0f1b7e111e746499cde98cbe0fbd96280
缘缘 <1253301988@qq.com>
2022-12-04 23:11:00 +0800

    C2G_EnterGameHandler

---
+++ Server/Hotfix/Demo/Account/Handler/C2G_EnterGameHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public class C2G_EnterGameHandler : AMRpcHandler<C2G_EnterGame, G2C_EnterGame>
+    {
+        protected override async ETTask Run(Session session, C2G_EnterGame request, G2C_EnterGame response, Action reply)
+        {
+        }
+    }
+}
