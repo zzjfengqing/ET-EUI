2b51c32959a98631f7084c926b6290333f66b8e3
缘缘 <1253301988@qq.com>
2022-12-05 17:01:53 +0800

    G2M_RequestExitGameHandler

---
+++ Server/Hotfix/Demo/Account/Handler/G2M_RequestExitGameHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public class G2M_RequestExitGameHandler : AMActorLocationRpcHandler<Unit, G2M_RequestExitGame, M2G_RequestExitGame>
+    {
+        protected override async ETTask Run(Unit unit, G2M_RequestExitGame request, M2G_RequestExitGame response, Action reply)
+        {
+            //TODO: 保存玩家数据到数据库,执行相关下线操作
+            reply();
+            //正是释放Unit
+            await unit.RemoveLocation();
+            UnitComponent unitComponent = unit.DomainScene().GetComponent<UnitComponent>();
+            unitComponent.Remove(unit.Id);
+        }
+    }
+}
