3c4a1c3311733de5f1f215cc83e2a7a12d618c51
缘缘 <1253301988@qq.com>
2022-12-06 22:41:21 +0800

    Other2UnitCache_DeleteUnitHandler

---
+++ Server/Hotfix/Demo/UnitCache/Handler/Other2UnitCache_DeleteUnitHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ET
+{
+    public class Other2UnitCache_DeleteUnitHandler : AMActorRpcHandler<Scene, Other2UnitCache_DeleteUnit, UnitCache2Other_DeleteUnit>
+    {
+        protected override async ETTask Run(Scene scene, Other2UnitCache_DeleteUnit request, UnitCache2Other_DeleteUnit response, Action reply)
+        {
+            UnitCacheComponent unitCacheComponent = scene.GetComponent<UnitCacheComponent>();
+            unitCacheComponent.Delete(request.UnitId);
+            reply();
+            await ETTask.CompletedTask;
+        }
+    }
+}
