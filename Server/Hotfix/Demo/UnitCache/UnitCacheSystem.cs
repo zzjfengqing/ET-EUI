8f2274e515f8a62ab6c0360baf651bfba8915571
缘缘 <1253301988@qq.com>
2022-12-06 22:40:59 +0800

    UnitCacheSystem

---
+++ Server/Hotfix/Demo/UnitCache/UnitCacheSystem.cs
@@ -0,0 +1,56 @@
+﻿namespace ET
+{
+    [FriendClass(typeof(UnitCache))]
+    public static class UnitCacheSystem
+    {
+        public class UnitCacheAwakeSystem : AwakeSystem<UnitCache>
+        {
+            public override void Awake(UnitCache self)
+            {
+                foreach (var entity in self.CacheComponents.Values)
+                {
+                    entity.Dispose();
+                }
+                self.CacheComponents.Clear();
+                self.EntityName = null;
+            }
+        }
+
+        public static void AddOrUpdate(this UnitCache self, Entity entity)
+        {
+            if (entity is null)
+                return;
+            if (self.CacheComponents.TryGetValue(entity.Id, out var oldEntity))
+            {
+                if (oldEntity != entity)
+                {
+                    oldEntity.Dispose();
+                }
+                self.CacheComponents.Remove(entity.Id);
+            }
+            self.CacheComponents[entity.Id] = entity;
+        }
+
+        public static void Delete(this UnitCache self, long unitId)
+        {
+            if (self.CacheComponents.TryGetValue(unitId, out var entity))
+            {
+                entity.Dispose();
+                self.CacheComponents.Remove(unitId);
+            }
+        }
+
+        public static async ETTask<Entity> Get(this UnitCache self, long unitId)
+        {
+            if (!self.CacheComponents.TryGetValue(unitId, out var entity))
+            {
+                entity = await DBManagerComponent.Instance.GetZoneDB(self.DomainZone()).Query<Entity>(unitId, self.EntityName);
+                if (entity != null)
+                {
+                    self.AddOrUpdate(entity);
+                }
+            }
+            return entity;
+        }
+    }
+}
--- Server/Model/Demo/UnitCache/UnitCacheComponent.cs
+++ Server/Model/Demo/UnitCache/UnitCacheComponent.cs
@@ -4,7 +4,7 @@ namespace ET
 {
     [ComponentOf(typeof(Scene))]
     [ChildType(typeof(UnitCache))]
-    public class UnitCacheComponent : Entity, IAwake
+    public class UnitCacheComponent : Entity, IAwake, IDestroy
     {
         public List<string> UnitCacheNames = new List<string>();
         public Dictionary<string, UnitCache> UnitCaches = new Dictionary<string, UnitCache>();
