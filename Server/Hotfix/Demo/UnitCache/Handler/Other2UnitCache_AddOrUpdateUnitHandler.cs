89975a3dd2ccde0a0313572fc4c7f5316f254c7b
缘缘 <1253301988@qq.com>
2022-12-06 22:41:11 +0800

    Other2UnitCache_AddOrUpdateUnitHandler

---
+++ Server/Hotfix/Demo/UnitCache/Handler/Other2UnitCache_AddOrUpdateUnitHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ET
+{
+    public class Other2UnitCache_AddOrUpdateUnitHandler : AMActorRpcHandler<Scene, Other2UnitCache_AddOrUpdateUnit, UnitCache2Other_AddOrUpdateUnit>
+    {
+        protected override async ETTask Run(Scene scene, Other2UnitCache_AddOrUpdateUnit request, UnitCache2Other_AddOrUpdateUnit response, Action reply)
+        {
+            UPdateUnitCacheAsync(scene, request, response);
+            reply();
+            await ETTask.CompletedTask;
+        }
+
+        private static async void UPdateUnitCacheAsync(Scene scene, Other2UnitCache_AddOrUpdateUnit request, UnitCache2Other_AddOrUpdateUnit response)
+        {
+            var unitCacheCompnent = scene.GetComponent<UnitCacheComponent>();
+            using (ListComponent<Entity> entityList = ListComponent<Entity>.Create())
+            {
+                for (int i = 0; i < request.EntityTypes.Count; i++)
+                {
+                    Type type = Game.EventSystem.GetType(request.EntityTypes[i]);
+                    Entity entity = MongoHelper.FromBson(type, request.EntityBytes[i]) as Entity;
+                    entityList.Add(entity);
+                }
+                await unitCacheCompnent.AddOrUpdate(request.UnitId, entityList);
+            }
+        }
+    }
+}
